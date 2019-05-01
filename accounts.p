5adec6f149107b35b1eb6fcc8b575ac34e5e940d
9921459 <mediamanmorrow@gmail.com>
2019-05-01 13:53:57 -0700

    Started to create user accounts

---
+++ accounts.p
@@ -0,0 +1,18 @@
+(lp0
+(i__main__
+User
+p1
+(dp2
+S'username'
+p3
+S'johnhenry99'
+p4
+sS'password'
+p5
+S'dragon'
+p6
+sS'name'
+p7
+S'John Henry'
+p8
+sba.
--- main.py
+++ main.py
@@ -1,40 +1,49 @@
-accounts = {
-    "steve": {
-        "name": "Steven",
-        "username": "steven99",
-        "password": "dragon",
-    }
+import pickle
 
-}
+try:
+    accounts = pickle.load(open("accounts.p", "rb"))
+    events = pickle.load(open("events.p", "rb"))
+    print("loaded accounts")
+    print(accounts)
 
-users = []
-
+except:
+    # accounts = {
+    #     "steve": {
+    #         "name": "Steven",
+    #         "username": "steven99",
+    #         "password": "dragon",
+    #     }
+    # }
+    accounts = []
+    print(accounts)
+    print()
 
 width = 50
+
+
 class User:
 
     """
     This class handles all the user functions and variables. This class is then stored in the accounts list and saved.
     """
 
-    def __init__(self, name, username, password):
+    def __init__(self, name=None, username=None, password=None):
         self.name = name
         self.username = username
         self.password = password
 
+    def create_account(self):
+        self.name = input("What is your full name?\n:")
+        self.username = input("What would you like your username to be?\n:")
 
+        while True:
+            self.password = input("Create a password:\n:")
+            confirm = input("Confirm your password:\n:")
+            if self.password == confirm:
+                print("Account Created successfully!")
 
     def print_stats(self):
         print(self.name, self.username, self.password)
-
-    def block_text(self, text, length=50):
-        final_text = ""
-        while len(text) >= length:
-            final_text += text[0:length] + "\n"
-            text = text[length:]
-        return final_text
-
-
 
     def create_event(self, title, date, description, agemin, agemax):
         self.title = title
@@ -51,7 +60,52 @@ class User:
         print("\nRecommended for ages {} - {}.".format(self.agemin, self.agemax))
 
 
-steve = User("Steve", "steve99", "dragon")
-users.append(steve)
-steve.create_event("Party at Steve's", "July 12th, 3:30pm", "Come to Steves house for an absolute rager! It's totally wild and will be the night of your life!", 16, 18)
-steve.print_event()
+class Control:
+
+    def __init__(self, accounts, user=None):
+        self.user = user
+        self.accounts = accounts
+
+    def welcome(self):
+        print("Welcome Campbell River Events!")
+        print("Would you like to sign in?\n")
+        login = self.yesno()
+        if login:
+            self.login(self.accounts)
+
+    def yesno(self):
+        while True:
+            answer = input(": ").lower()
+            if "y" in answer:
+                return True
+                break
+
+            if "n" in answer:
+                return False
+                break
+
+            else:
+                print("Incorrect format entered. Try typing 'yes' or 'no'. Or, 'y' or 'n' for short.")
+
+    def login(self):
+        username = input("Username: ")
+        password = input("Password: ")
+
+        for user in self.accounts:
+            if username == user.username:
+                if password == user.password:
+                    print("signed in successfully")
+                    return user
+            else:
+                print("That username is not recorded in our database. If you would like to create an account, type: signup.")
+
+# steve.create_event("Party at Steve's", "July 12th, 3:30pm", "Come to Steves house for an absolute rager! It's totally wild and will be the night of your life!", 16, 18)
+
+control = Control(accounts)
+
+
+user = User("John Henry", "johnhenry99", "dragon")
+user.print_stats()
+accounts.append(user)
+print(accounts)
+pickle.dump(accounts, open("accounts.p", "wb"))
