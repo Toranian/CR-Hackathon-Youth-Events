8f7681e9fe56c68863544b72eeee1e030b0a5081
9921459 <mediamanmorrow@gmail.com>
2019-05-02 13:23:25 -0700

    Added Events

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pickle, time, os
 from objects import *
 
+# Hey! You there! If you are to edit this code, make sure you are using the pep 8
+# style guide!  https://www.python.org/dev/peps/pep-0008/
+
 
 # Loads the accounts and events from the accounts.txt and events.txt files.
 if __name__ == "__main__":
 
     try:
         accounts = pickle.load(open("accounts.txt", "rb"))
-        # events = pickle.load(open("events.txt", "rb"))
+        events = pickle.load(open("events.txt", "rb"))
         print("\n\n\n\n")
         print("_"*50)
         print(">>> Loaded accounts successfully!\n")
-        print(accounts)
         for user in accounts:
             print(user.username)
-        # print("accounts:", accounts)
+        print(">>> Loaded events successfully!")
+        for event in events:
+            print(event.title)
         print("_"*50)
         print("\n\n\n\n")
 
@@ -27,53 +31,86 @@ if __name__ == "__main__":
         ]
         pickle.dump(accounts, open("accounts.txt", "wb"))
         print(accounts)
-        events = []
+        events = ["Test"]
+        pickle.dump(events, open("events.txt", "wb"))
 
 
 # Initialize the control class which has most of the functions within it.
 control = Control(accounts)
 command_list = ["login", "signup", "home", "quit", "events", "userstats", "createevent"]
 
-# starts the mainloop
-loop = True
 
-# asks if the user has any accounts
-has_account = control.yesno("Do you have an account?\n: ")
+# Asks if the user has any account
+has_account = control.yesno("Do you have an account?"); control.clear_screen(0)
 
+# If the user has an account, they will be signed in
 if has_account:
     active_user = control.login()
 
+# creates a new account and makes the active_user live
 if not has_account:
-        name, username, password, age = create_account(accounts)
-        new_user = User(name, username, password, age)
-        accounts.append(new_user)
-        print("created a new account!", new_user.name)
-        print(accounts)
-        pickle.dump(accounts, open("accounts.txt", "wb"))
-        active_user = new_user
-        print("NO ERRORS!")
+        try:
+            name, username, password, age = control.create_account()
+            new_user = User(name, username, password, age)
+            accounts.append(new_user)
+            pickle.dump(accounts, open("accounts.txt", "wb"))
+            active_user = new_user
+            control.output_message("Account '{}' created successfully.".format(active_user.username))
 
+        except Exception as e:
+            control.output_message("Account was not created successfully.\n{}".format(e))
 
 # Main loop of the whole program! Will let the user select what they want.
+loop = True
 while loop:
 
+    # Let's the user continue the program by hitting enter on the keyboard.
+    control.continue_loop()
+    control.clear_screen(0)
+
     # Get the command from the user
-    command = input("\nWhat would you like to do? Type (h)elp for a list of commands.\n: ").lower().strip()
+    control.output_message("What would you like to do? Type (h)elp for a list of commands.")
+    command = control.input_message(" ").lower().replace(" ", "")
+    print(command)
+
+    if command == "":
+        continue
 
     if command not in command_list:
         print("Command not recognized. Try typing (h)elp for a list of commands.\n")
-        clear_screen()
+        control.clear_screen()
 
     if "q" in command:
         print("Exiting the program!")
         quit()
 
-    if "userstats" in command:
-        active_user.print_stats()
+    # Create a new account
+    if "signup" in command:
+        control.output_message("Creating a new user")
+        try:
+            name, username, password, age = control.create_account()
+            new_user = User(name, username, password, age)
+            accounts.append(new_user)
+            pickle.dump(accounts, open("accounts.txt", "wb"))
+            active_user = new_user
+            control.output_message("Account '{}' created successfully.".format(active_user.username))
 
+        except Exception as e:
+            control.output_message("Account was not created successfully.\n{}".format(e))
+
+    # Print the active users stats
+    if "userstats" in command:
+        control.clear_screen()
+        active_user.get_stats()
+
+    # Show all the available commands
     if "help" in command:
         for cmd in command_list:
             print(cmd)
 
     if "createevent" in command:
-        pass
+         title, description, time, location, created_by = control.create_event(active_user)
+         new_event = Event(title, description, time, location, created_by)
+         events.append(new_event)
+         print(events)
+         pickle.dump(events, open("events.txt", "wb"))
--- objects.py
+++ objects.py
@@ -1,11 +1,10 @@
 import os, time, pickle
 
-# clears the screen after a certain amount of time. Default value is 0.5 seconds.
-def clear_screen(wait=0.5):
-    time.sleep(wait)
-    os.system("cls")
+# Hey! You there! If you are to edit this code, make sure you are using the pep 8
+# style guide!  https://www.python.org/dev/peps/pep-0008/
 
-# user accounts
+
+# User accounts
 class User:
 
     """
@@ -20,7 +19,7 @@ class User:
         self.password = password
         self.age = age
 
-    def print_stats(self):
+    def get_stats(self):
         print("Stats:\n{}\nName: {}\nUsername: {}\nPassword: {}\nAge: {}\n{}".format("_"*50, self.name, self.username, self.password, self.age, "_"*50))
 
     def create_event(self, title, date, description, agemin, agemax):
@@ -38,7 +37,21 @@ class User:
         print("\nRecommended for ages {} - {}.".format(self.agemin, self.agemax))
 
 
-# the main control class of the program.
+# Event objects.
+class Event:
+
+    def __init__(self, title, description, time, location, created_by):
+        self.title = title
+        self.description = description
+        self.time = time
+        self.location = location
+        self.created_by = created_by
+
+    def display(self):
+        print("Event display")
+
+
+# The main control handler class of the program
 class Control:
 
     """
@@ -50,16 +63,11 @@ class Control:
         self.accounts = accounts
         self.width = 50
 
-    def welcome(self):
-        message = "Welcome to Campbell River Events!".center(self.width); print(message)
-        print("_"*self.width)
-        print("Would you like to sign in?\n")
-        login = self.yesno()
-        return login
-
+    # returns a true or false value based on the user input
     def yesno(self, message):
         while True:
-            answer = input("{}".format(message)).lower()
+            answer = self.input_message(message).lower()
+            # answer = input("{}".format(message)).lower()
             if "y" in answer:
                 return True
                 break
@@ -71,6 +79,7 @@ class Control:
             else:
                 print("Incorrect format entered. Try typing 'yes' or 'no'. Or, 'y' or 'n' for short.")
 
+    # Logs the user in. Checks the database to see if the username and password match the records.
     def login(self):
         username = input("Username: ")
         password = input("Password: ")
@@ -78,36 +87,67 @@ class Control:
         for user in self.accounts:
             if username == user.username:
                 if password == user.password:
-                    print("signed in successfully")
+                    self.clear_screen(0)
+                    self.output_message("Signed in successfully")
                     return user
             else:
                 print("That username is not recorded in our database. If you would like to create an account, type: signup.")
 
+    # returns the name, username and password after the user creates an account.
+    def create_account(self):
 
-# returns the name, username and password after the user creates an account.
-def create_account(accounts):
+        name = input("\nWhat is your full name?\n: ")
 
-    name = input("\nWhat is your full name?\n: ")
+        # create the username. Repeat if it is already in the database!
+        while True:
+            username = input("\nWhat would you like your username to be?\n: ")
+            for user in self.accounts:
+                if user.username == username:
+                    print("That username already exists in our database. Please try again!")
+            break
 
-    # create the username. Repeat if it is already in the database!
-    while True:
-        username = input("\nWhat would you like your username to be?\n: ")
-        for user in accounts:
-            if user.username == username:
-                print("That username already exists in our database. Please try again!")
-        break
+        age = input("\nHow old are you?:\n: ")
 
-    age = input("\nHow old are you?:\n: ")
+        # make sure the passwords match!
+        while True:
+            password = input("Create a password:\n:")
+            confirm = input("Confirm your password:\n:")
+            if password == confirm:
+                print("Account Created successfully!")
+                clear_screen()
+                return name, username, password, age
 
-    # make sure the passwords match!
-    while True:
-        password = input("Create a password:\n:")
-        confirm = input("Confirm your password:\n:")
-        if password == confirm:
-            print("Account Created successfully!")
-            clear_screen()
-            return name, username, password, age
+    # clears the screen after a certain amount of time. Default value is 0.5 seconds.
+    def clear_screen(self, wait=0.5):
+        time.sleep(wait)
 
+        os.system("cls")
 
-def create_event(events):
-    pass
+        os.system("clear")
+
+    # Output message
+    def output_message(self, message):
+        print("\n")
+        print("_"*len(message))
+        print("{}".format(message))
+        print("_"*len(message))
+        print("\n")
+
+    def input_message(self, message):
+        return input("{}\n-> ".format(message))
+
+    # creates the event
+    def create_event(self, active_user):
+        # title = input("What would you like the title of your event to be?\n: ")
+        # description = input("What would you like the description of your event to be?\n: ")
+        title = self.input_message("What would you like the title of your event to be?")
+        description = self.input_message("What would you like the description of your event to be?")
+        time = self.input_message("What time would you like the event to be at?")
+        location = self.input_message("Where is this event being held (location)?")
+        created_by = active_user.username
+
+        return title, description, time, location, created_by
+
+    # Continue the loop after a user returns
+    def continue_loop(self):
+        input("\nPress enter to contine.\n")
