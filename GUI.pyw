f2bc804aba0b50836530483e7d7974a11352ad32
Hackathon <e_posner@outlook.com>
2019-05-03 13:03:17 -0700

    Added GUI

--- GUI.pyw
+++ GUI.pyw
@@ -1,17 +1,95 @@
 import tkinter as tk
 import pickle
 
+def changetologinframe():
+    login_frame.tkraise()
+    login_frame.pack()
+    register_frame.pack_forget()
+
+def changetoregisterframe():
+    print("changing to register frame")
+    register_frame.tkraise()
+    register_frame.pack()
+    login_frame.pack_forget()
+
+def login():
+    pass
+
+def register():
+    pass
+
 root = tk.Tk()
 root.title("Campbell Rivents")
 root.configure(bg = "grey25")
 root.geometry("600x400")
 
+#frame for main menu
 main_frame = tk.Frame(root)
 
-eventsLabel = tk.Label(root, text="test", fg="black", bg="grey25")
+eventsLabel = tk.Label(main_frame, text="test", fg="white", bg="grey25", font = ('consolas', 10, 'bold'))
 eventsLabel.place(relx=.5, rely=.6, anchor="center")
 
-main_frame.tkraise()
+#frame for login screen
+login_frame = tk.Frame(root, bg="grey25", width=600, height=400)
+#login_frame.grid_propagate(False)
+
+login_label = tk.Label(login_frame, text = "LOGIN", fg = "white", bg = "grey25", font = ('consolas', 50, 'bold'))
+login_label.place(relx=.5, rely=.4, anchor="center")
+usernamelabel = tk.Label(login_frame, text="Username", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+usernamelabel.place(relx=.5, rely=.55, anchor="center")
+usernamebox = tk.Entry(login_frame, width="20")
+usernamebox.place(relx=.5, rely=.6, anchor="center")
+passwordlabel = tk.Label(login_frame, text="Password", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+passwordlabel.place(relx=.5, rely=.75, anchor="center")
+passwordbox = tk.Entry(login_frame, width="20")
+passwordbox.place(relx=.5, rely=.8, anchor="center")
+changetoregisterbutton = tk.Button(login_frame, text="register", font = ('consolas', 10, 'bold'), bg="white", fg="black", command=changetoregisterframe)
+changetoregisterbutton.place(relx=.45, rely=.9, anchor="center")
+loginbutton = tk.Button(login_frame, text="login", font = ('consolas', 10, 'bold'), bg="white", fg="black", command=login)
+loginbutton.place(relx=.55, rely=.9, anchor="center")
+
+login_frame.pack()
+
+##
+login_frame.pack_forget()
+##
+
+register_frame = tk.Frame(root, bg="grey25", width=600, height=400)
+
+register_label = tk.Label(register_frame, text = "REGISTER", fg = "white", bg = "grey25", font = ('consolas', 50, 'bold'))
+register_label.place(relx=.5, rely=.2, anchor="center")
+
+fullnamelabel = tk.Label(register_frame, text="Full Name", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+fullnamelabel.place(relx=.5, rely=.4, anchor="center")
+fullnamebox = tk.Entry(register_frame, width="20")
+fullnamebox.place(relx=.5, rely=.45, anchor="center")
+
+registerusernamelabel = tk.Label(register_frame, text="Username", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+registerusernamelabel.place(relx=.5, rely=.5, anchor="center")
+registerusernamebox = tk.Entry(register_frame)
+registerusernamebox.place(relx=.5, rely=.55, anchor="center")
+
+ageLabel = tk.Label(register_frame, text="Age", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+ageLabel.place(relx=.5, rely=.6, anchor="center")
+ageBox = tk.Entry(register_frame, width="5")
+ageBox.place(relx=.5, rely=.65, anchor="center")
+
+registerpasswordlabel = tk.Label(register_frame, text="Password", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+registerpasswordlabel.place(relx=.5, rely=.7, anchor="center")
+registerpasswordbox = tk.Entry(register_frame)
+registerpasswordbox.place(relx=.5, rely=.75, anchor="center")
+confirmpasswordlabel = tk.Label(register_frame, text="Confirm Password", font = ('consolas', 10, 'bold'), bg="grey25", fg="white")
+confirmpasswordlabel.place(relx=.5, rely=.8, anchor="center")
+confirmpasswordbox = tk.Entry(register_frame, width="20")
+confirmpasswordbox.place(relx=.5, rely=.85, anchor="center")
+registerbutton = tk.Button(register_frame, text="register", font = ('consolas', 10, 'bold'), bg="white", fg="black", command=register)
+registerbutton.place(relx=.45, rely=.95, anchor="center")
+changetologinbutton = tk.Button(register_frame, text="login", font = ('consolas', 10, 'bold'), bg="white", fg="black", command=changetologinframe)
+changetologinbutton.place(relx=.55, rely=.95, anchor="center")
+
+register_frame.pack()
+
+register_frame.tkraise()
 
 if __name__ == "__main__":
 
---
+++ gui_objects.py
@@ -0,0 +1,162 @@
+import os, time, pickle
+
+# Hey! You there! If you are to edit this code, make sure you are using the pep 8
+# style guide!  https://www.python.org/dev/peps/pep-0008/
+
+
+# User accounts
+class User:
+
+    """
+    This class handles all the user functions and variables. This class is then stored in the accounts list and saved.
+    """
+
+    def __init__(self, name=None, username=None, password=None, age=None):
+
+        self.width = 50
+        self.name = name
+        self.username = username
+        self.password = password
+        self.age = age
+
+    def get_stats(self):
+        print("Stats:\n{}\nName: {}\nUsername: {}\nPassword: {}\nAge: {}\n{}".format("_"*50, self.name, self.username, self.password, self.age, "_"*50))
+
+    def create_event(self, title, date, description, agemin, agemax):
+        self.title = title
+        self.date = date
+        self.description = self.block_text(description)
+        print(self.description)
+        self.agemin = agemin
+        self.agemax = agemax
+
+
+# Event objects.
+class Event:
+
+    """
+    Handles the events and displays them as well into objects.
+    """
+
+    def __init__(self, title, description, time, location, created_by):
+        self.title = title
+        self.description = description
+        self.time = time
+        self.location = location
+        self.created_by = created_by
+
+    def display(self):
+
+        bottom_text = f"\nDate: {self.time}, Location: {self.location}, Event created by {self.created_by}"
+
+        if len(self.description) > len(bottom_text):
+            length = len(self.description)
+        else:
+            length = len(bottom_text)
+
+        print("_" * length)
+        print(self.title.center(length))
+        print("_" * length)
+        print("")
+        print(self.description)
+        print(bottom_text)
+
+
+
+# The main control handler class of the program
+class Control:
+
+    """
+    Contains many of the most critical functions in the program.
+    """
+
+    def __init__(self, accounts, user=None):
+        self.user = user
+        self.accounts = accounts
+        self.width = 50
+
+    # returns a true or false value based on the user input
+    def yesno(self, message):
+        while True:
+            answer = self.input_message(message).lower()
+            # answer = input("{}".format(message)).lower()
+            if "y" in answer:
+                return True
+
+            if "n" in answer:
+                return False
+
+            else:
+                print("Incorrect format entered. Try typing 'yes' or 'no'. Or, 'y' or 'n' for short.")
+
+    # Logs the user in. Checks the database to see if the username and password match the records.
+    def login(self):
+        username = input("Username: ")
+        password = input("Password: ")
+
+        for user in self.accounts:
+            if username == user.username:
+                if password == user.password:
+                    self.clear_screen(0)
+                    self.output_message("Signed in successfully")
+                    return user
+            else:
+                print("That username is not recorded in our database. If you would like to create an account, type: signup.")
+
+    # returns the name, username and password after the user creates an account.
+    def create_account(self):
+
+        name = input("\nWhat is your full name?\n: ")
+
+        # create the username. Repeat if it is already in the database!
+        while True:
+            username = input("\nWhat would you like your username to be?\n: ")
+            for user in self.accounts:
+                if user.username == username:
+                    print("That username already exists in our database. Please try again!")
+            break
+
+        age = input("\nHow old are you?:\n: ")
+
+        # make sure the passwords match!
+        while True:
+            password = input("Create a password:\n:")
+            confirm = input("Confirm your password:\n:")
+            if password == confirm:
+                print("Account Created successfully!")
+                self.clear_screen()
+                return name, username, password, age
+
+    # clears the screen after a certain amount of time. Default value is 0.5 seconds.
+    def clear_screen(self, wait=0.5):
+        time.sleep(wait)
+        os.system("cls")
+        # os.system("clear")
+
+    # Output message
+    def output_message(self, message):
+        print("\n")
+        print("_"*len(message))
+        print("{}".format(message))
+        print("_"*len(message))
+        print("\n")
+
+    # Writes a message and takes input. Returns the input
+    def input_message(self, message):
+        return input("{}\n-> ".format(message))
+
+    # creates the event
+    def create_event(self, active_user):
+        # title = input("What would you like the title of your event to be?\n: ")
+        # description = input("What would you like the description of your event to be?\n: ")
+        title = self.input_message("What would you like the title of your event to be?")
+        description = self.input_message("What would you like the description of your event to be?")
+        time = self.input_message("What is the time and date of this event?")
+        location = self.input_message("Where is this event being held (location)?")
+        created_by = active_user.username
+
+        return title, description, time, location, created_by
+
+    # Continue the loop after a user returns
+    def continue_loop(self):
+        input("\nPress enter to contine.\n")
